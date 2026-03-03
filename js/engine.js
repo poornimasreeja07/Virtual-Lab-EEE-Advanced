da450c37231fc1c23ae41ad3ae852d4274f0396e
poornimasreeja07 <poornimasreeja07@gmail.com>
2026-03-03 20:58:20 +0530

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,32 @@
+class LabEngine {
 
+    constructor() {
+        this.voltage = 0;
+        this.init();
+    }
+
+    init() {
+        console.log("Lab Engine Initialized");
+        this.setupVoltageControl();
+    }
+
+    setupVoltageControl() {
+        const slider = document.getElementById("voltageSlider");
+        const display = document.getElementById("voltageDisplay");
+
+        slider.addEventListener("input", (e) => {
+            this.voltage = parseInt(e.target.value);
+            display.innerText = this.voltage + " V";
+
+            this.updateSimulation();
+        });
+    }
+
+    updateSimulation() {
+        console.log("Voltage Updated:", this.voltage);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    new LabEngine();
+});
